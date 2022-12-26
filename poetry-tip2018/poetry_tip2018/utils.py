fddb873dd48eec7ba15f32c1966d569c7d4374a8
alikaraali <alixkaraali@gmail.com>
2022-12-26 14:42:29 +0000

    some classes are added

--- poetry-tip2018/poetry_tip2018/utils.py
+++ poetry-tip2018/poetry_tip2018/utils.py
@@ -1,5 +1,25 @@
-#TODO : write utility functions here.
+import cv2
 
-import json
 
-print('utils functions')
+class Image:
+    def __init__(self, image_path=None):
+        if image_path != None:
+            self.image = cv2.imread(image_path)
+            self.height = 0
+            self.width = 0
+        else:
+            self.image = None
+            self.height = 0
+            self.width = 0
+
+    def add_image_path(self, image_path):
+        self.image_path = image_path
+
+    def _find_edges(self):
+        self.edge = Edge(self.image)
+
+
+class Edge:
+    def __init__(self, Image):
+        self.edge_map = cv2.find_edges(Image.image)
+
